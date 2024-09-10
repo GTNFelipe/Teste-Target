0558219372f1a1abe80a2d6dbf27fba67cef3104
Felipe Santana <felipestartt@gmail.com>
2024-09-10 19:47:09 -0300

    teste5.py

--- teste5.py
+++ teste5.py
@@ -3,24 +3,24 @@ def identificando_luz():
     # 'off' = desligado, 'on' = ligado
     # A lâmpada que estava ligada por mais tempo vai estar quente
     # As lâmpadas inicialmente estão apagadas e frias
-    lights = {'A': 'cold', 'B': 'cold', 'C': 'cold'}
+    lampada = {'A': 'fria', 'B': 'fria', 'C': 'fria'}
     
     # Fase 1: Liga o primeiro interruptor e deixa ligado por um tempo
     # (Simula o aquecimento)
-    lights['A'] = 'warm'
+    lampada['A'] = 'esuentou'
     
     # Fase 2: Desliga o primeiro interruptor e liga o segundo
     # (Fica ligado até a checagem)
     # Simulando o estado das lâmpadas depois
-    lights['A'] = 'cold'  # Primeiro interruptor desligado
-    lights['B'] = 'warm'  # Segundo interruptor ligado
-    lights['C'] = 'cold'  # Terceiro interruptor permanece desligado
+    lampada['A'] = 'fria'  # Primeiro interruptor desligado
+    lampada['B'] = 'quente'  # Segundo interruptor ligado
+    lampada['C'] = 'fria'  # Terceiro interruptor permanece desligado
     
     # Checando os estados das lâmpadas
     results = {
-        'Lâmpada controlada pelo primeiro interruptor': 'A' if lights['A'] == 'warm' else 'não identificado',
-        'Lâmpada controlada pelo segundo interruptor': 'B' if lights['B'] == 'warm' else 'não identificado',
-        'Lâmpada controlada pelo terceiro interruptor': 'C' if lights['C'] == 'cold' else 'não identificado'
+        'Lâmpada controlada pelo primeiro interruptor': 'A' if lampada['A'] == 'qquente' else 'não identificado',
+        'Lâmpada controlada pelo segundo interruptor': 'B' if lampada['B'] == 'quente' else 'não identificado',
+        'Lâmpada controlada pelo terceiro interruptor': 'C' if lampada['C'] == 'fria' else 'não identificado'
     }
     
     return results
@@ -29,3 +29,4 @@ def identificando_luz():
 results = identificando_luz()
 for description, lamp in results.items():
     print(f"{description}: {lamp}")
+
